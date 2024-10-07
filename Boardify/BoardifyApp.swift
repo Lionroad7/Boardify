1b5774b55dd3d4ea6039fef69f418ac0279a723f
Islombek Akmadjonov <nexiar280hp@gmail.com>
2024-10-07 16:23:43 +0500

    Initial Commit

---
+++ Boardify/BoardifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardifyApp.swift
+//  Boardify
+//
+//  Created by islombek on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BoardifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
